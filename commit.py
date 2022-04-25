7565e2bf83a66d78b83e11b31bc315063502e3d1
nvanslyke <95662921+nvanslyke@users.noreply.github.com>
2022-04-25 17:05:26 -0500

    initial commit

---
+++ commit.py
@@ -0,0 +1,34 @@
+import git
+from git import Repo
+from datetime import datetime
+from datetime import date
+
+changing_file = "numCommits.txt"
+
+def update():
+    
+    today = date.today()
+    
+    today_date = today.strftime("%b-%d-%Y")
+    
+    now = datetime.now()
+    current_time = now.strftime("%H:%M:%S")
+    
+    new_write = ("Day of Last Commit - " + today_date + "\n"
+                 "Time of Last Commit - " + current_time)
+
+   # print(new_write)
+    
+    with open(changing_file, 'w') as file:
+        file.write(new_write)
+    
+
+def commit():
+    
+    repo = git.Repo("C:\\Users\\nvans\\Desktop\\Github")
+    repo.index.add(changing_file)
+    repo.index.commit("Commit from Script")
+    origin = repo.remote(name ='origin')
+    origin.push()
+
+commit()
